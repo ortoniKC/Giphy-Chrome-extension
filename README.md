f0072a5f19406080090ba337059a405bd51cbc67
Koushik <koushik350@gmail.com>
2020-10-12 20:59:46 +0530

    🚀 RELEASE: Giphy

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Giphy-Chrome-extension
+
 Chrome extension to download GIF's using the Giphy free API
+
+Links
+
+- [To generate Gipgy API](https://developers.giphy.com/)
+- [Spectre CSS to design](https://picturepan2.github.io/spectre/)
+- [API Reference](https://giphy.api-docs.io/1.0/gifs/)
+
+## Video tutorial
+
+Giphy
---
+++ popup.js
@@ -0,0 +1,32 @@
+const api_key = 'VYDfcisuxzIB93uxfQEt66DwNt1DDTzg'
+const search = document.querySelector("#search");
+search.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        fetchImages(search.value)
+    }
+})
+function fetchImages(value) {
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${value}&limit=9&api_key=${api_key}`)
+        .then(res => res.json())
+        .then(jsonData => {
+            buildUI(jsonData)
+        })
+}
+function buildUI(response) {
+    // document.write(JSON.stringify(response))
+    let column = document.querySelector('.columns')
+    let len = response.data.length;
+    column.innerHTML = '';
+    for (let index = 0; index < len; index++) {
+        let url = response.data[index].images.original.url;
+        let img = document.createElement('img')
+        img.setAttribute('src', url);
+        column.appendChild(img)
+    }
+
+}
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.tagName === "IMG")
+        chrome.downloads.download({ url: e.target.src }, () => { })
+})
